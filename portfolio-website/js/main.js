b54d03a98e9774a425268b92d59a4a2893a06325
Zackary <21011429@hope.ac.uk>
2024-10-31 18:39:40 +0000

    - Added typewriter, satchel and desk to environment (properly transformed) 31/10/24 - 2

--- portfolio-website/js/main.js
+++ portfolio-website/js/main.js
@@ -1,10 +1,69 @@
 import * as BABYLON from '@babylonjs/core';
+import '@babylonjs/loaders/GLTF';
+import {Inspector} from '@babylonjs/inspector';
 
 const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas);
 
 const createScene = function() {
     const scene = new BABYLON.Scene(engine);
+
+    scene.createDefaultCameraOrLight(true, false, true);
+
+    // const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
+    //     size: 0.1,
+    //     faceColors: [
+    //         BABYLON.Color3.Blue()
+    //     ]
+    // }, scene);
+
+    // const ground = new BABYLON.MeshBuilder.CreateGround('', {
+    //     height: 10,
+    //     width: 10,
+    //     subdivisions: 15,
+    //     subdivisionsX: 10
+    // })
+    //
+    // ground.material = new BABYLON.StandardMaterial();
+    // ground.material.wireframe = true;
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'antique_desk.gltf',
+        scene,
+        function (meshes) {
+
+            const large_desk = meshes[0];
+            large_desk.position = new BABYLON.Vector3(0, 0, 3);
+        });
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'typewriter.gltf',
+        scene,
+        function (meshes) {
+
+            const typewriter = meshes[0];
+            typewriter.position = new BABYLON.Vector3(-0.1, 0.975, 3.4);
+            typewriter.rotation = new BABYLON.Vector3(0, Math.PI / 8, 0);
+            typewriter.scaling = new BABYLON.Vector3(0.5625, 0.5625, 0.5625);
+        });
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'antique_satchel.gltf',
+        scene,
+        function (meshes) {
+
+            const satchel = meshes[0];
+            satchel.position = new BABYLON.Vector3(-0.684, 0, 3.4);
+            satchel.rotation = new BABYLON.Vector3(0, Math.PI / 6, 0);
+            satchel.scaling = new BABYLON.Vector3(0.5625, 0.5625, 0.5625);
+        });
+
+
     return scene;
 }
 
@@ -13,3 +72,9 @@ const scene = createScene();
 engine.runRenderLoop(function() {
     scene.render();
 });
+
+window.addEventListener('resize', function() {
+   engine.resize();
+});
+
+Inspector.Show(scene, {});
