72cacab1c916a5b07e5d50ca7e13028759b3e274
Zackary <21011429@hope.ac.uk>
2024-11-01 00:20:52 +0000

    - Added most current models to environment including WIP bookshelf and room - Testing constraining camera movement and zooming 31/10/24 - 3

--- portfolio-website/js/main.js
+++ portfolio-website/js/main.js
@@ -9,23 +9,30 @@ const createScene = function() {
     const scene = new BABYLON.Scene(engine);
 
     scene.createDefaultCameraOrLight(true, false, true);
+    var camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(-0.8, 1.7, 1.34), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.attachControl(canvas, true);
+    camera.lowerRadiusLimit= 0.01;
+    camera.upperRadiusLimit = 0.01;
 
-    // const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
-    //     size: 0.1,
-    //     faceColors: [
-    //         BABYLON.Color3.Blue()
-    //     ]
-    // }, scene);
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'study.gltf',
+        scene,
+        function (meshes) {
 
-    // const ground = new BABYLON.MeshBuilder.CreateGround('', {
-    //     height: 10,
-    //     width: 10,
-    //     subdivisions: 15,
-    //     subdivisionsX: 10
-    // })
-    //
-    // ground.material = new BABYLON.StandardMaterial();
-    // ground.material.wireframe = true;
+            const study = meshes[0];
+            study.position = new BABYLON.Vector3(-3.147, 0, -0.015);
+            study.rotation = new BABYLON.Vector3(0, Math.PI/2, 0);
+            study.scaling = new BABYLON.Vector3(-4, 4, 4);
+
+            study.forEach((mesh) => {
+                if (mesh.material) {
+                    mesh.material.alpha = 1;
+                }
+            });
+        });
 
     BABYLON.SceneLoader.ImportMesh(
         '',
@@ -36,6 +43,31 @@ const createScene = function() {
 
             const large_desk = meshes[0];
             large_desk.position = new BABYLON.Vector3(0, 0, 3);
+
+            large_desk.forEach((mesh) => {
+                if (mesh.material) {
+                    mesh.material.alpha = 1;
+                }
+            });
+        });
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'large_bookshelf.gltf',
+        scene,
+        function (meshes) {
+
+            const large_bookshelf = meshes[0];
+            large_bookshelf.position = new BABYLON.Vector3(-1.345, 0, 2.807);
+            large_bookshelf.rotation = new BABYLON.Vector3(0, 0, 0);
+            large_bookshelf.scaling = new BABYLON.Vector3(1.675, 1.675, 1.675);
+
+            large_bookshelf.forEach((mesh) => {
+                if (mesh.material) {
+                    mesh.material.alpha = 1;
+                }
+            });
         });
 
     BABYLON.SceneLoader.ImportMesh(
@@ -46,10 +78,11 @@ const createScene = function() {
         function (meshes) {
 
             const typewriter = meshes[0];
-            typewriter.position = new BABYLON.Vector3(-0.1, 0.975, 3.4);
+            typewriter.position = new BABYLON.Vector3(0.5, 0.975, 3.1);
             typewriter.rotation = new BABYLON.Vector3(0, Math.PI / 8, 0);
-            typewriter.scaling = new BABYLON.Vector3(0.5625, 0.5625, 0.5625);
+            typewriter.scaling = new BABYLON.Vector3(-0.5625, 0.5625, 0.5625);
         });
+
     BABYLON.SceneLoader.ImportMesh(
         '',
         '/images-objects/',
@@ -58,9 +91,111 @@ const createScene = function() {
         function (meshes) {
 
             const satchel = meshes[0];
-            satchel.position = new BABYLON.Vector3(-0.684, 0, 3.4);
+            satchel.position = new BABYLON.Vector3(-0.25, 0, 3.1);
             satchel.rotation = new BABYLON.Vector3(0, Math.PI / 6, 0);
-            satchel.scaling = new BABYLON.Vector3(0.5625, 0.5625, 0.5625);
+            satchel.scaling = new BABYLON.Vector3(-0.5625, 0.5625, 0.5625);
+        });
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'rotary_phone.gltf',
+        scene,
+        function (meshes) {
+
+            const phone = meshes[0];
+            phone.position = new BABYLON.Vector3(1, 1, 1);
+            phone.rotation = new BABYLON.Vector3(0, 0, 0);
+            phone.scaling = new BABYLON.Vector3(-0.5625, 0.5625, 0.5625);
+        });
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'antique_desk_lamp.gltf',
+        scene,
+        function (meshes) {
+
+            const desk_lamp = meshes[0];
+            desk_lamp.position = new BABYLON.Vector3(-0.95, 0.975, 3.75);
+            desk_lamp.rotation = new BABYLON.Vector3(0, Math.PI * (5/3), 0);
+            desk_lamp.scaling = new BABYLON.Vector3(-0.5625, 0.5625, 0.5625);
+
+            desk_lamp.forEach((mesh) => {
+               if (mesh.material) {
+                   mesh.material.alpha = 1;
+               }
+            });
+
+        });
+    const desk_lamp_light = new BABYLON.SpotLight(
+      'spotLight',
+      new BABYLON.Vector3(-1.163, 1.468, 3.548),
+      new BABYLON.Vector3(68, -112.5, -45),
+      Math.PI,
+      1,
+      scene
+    );
+    desk_lamp_light.intensity = 10;
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'antique_map_1.gltf',
+        scene,
+        function (meshes) {
+
+            const map_1 = meshes[0];
+            map_1.position = new BABYLON.Vector3(-0.9, 0.975, 3.75);
+            map_1.rotation = new BABYLON.Vector3(0, Math.PI, 0);
+            map_1.scaling = new BABYLON.Vector3(-0.5625, 0.5625, 0.5625);
+
+            map_1.forEach((mesh) => {
+                if (mesh.material) {
+                    mesh.material.alpha = 1;
+                }
+            });
+
+        });
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'scale_arm_compass.gltf',
+        scene,
+        function (meshes) {
+
+            const scale_arm_compass = meshes[0];
+            scale_arm_compass.position = new BABYLON.Vector3(-0.66, 0.933, 3.54);
+            scale_arm_compass.rotation = new BABYLON.Vector3(Math.PI * 1.5, Math.PI * (5/3), 0);
+            scale_arm_compass.scaling = new BABYLON.Vector3(-0.15, 0.15, 0.15);
+
+            scale_arm_compass.forEach((mesh) => {
+                if (mesh.material) {
+                    mesh.material.alpha = 1;
+                }
+            });
+
+        });
+
+    BABYLON.SceneLoader.ImportMesh(
+        '',
+        '/images-objects/',
+        'antique_compass.gltf',
+        scene,
+        function (meshes) {
+
+            const compass = meshes[0];
+            compass.position = new BABYLON.Vector3(-1.3, 0.975, 3.35);
+            compass.rotation = new BABYLON.Vector3(0, Math.PI * (7/10), 0);
+            compass.scaling = new BABYLON.Vector3(-0.18, 0.18, 0.18);
+
+            scale_arm_compass.forEach((mesh) => {
+                if (mesh.material) {
+                    mesh.material.alpha = 1;
+                }
+            });
+
         });
 
 
