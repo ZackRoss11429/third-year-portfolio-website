1e7e9da3873b5f9877bdf517956afeebbf96fb47
ZackRoss11429 <21011429@hope.ac.uk>
2025-03-01 01:34:25 +0000

    - Proper display of book texture when clicking on interactable book. - Custom Minecraft font loaded from fonts folder - New directory for all book content which is asynchronously loaded from .txt files to the screen
    1/3/25 - 1

--- portfolio-website/js/book_content.js
+++ portfolio-website/js/book_content.js
@@ -1,36 +1,48 @@
 import * as BABYLON from "@babylonjs/core";
 import * as GUI from "@babylonjs/gui";
+import {Button, AdvancedDynamicTexture, Image} from "@babylonjs/gui";
 
 class interactableBook {
     constructor(title, pageCount) {
         this.title = title;
         this.pageCount = pageCount;
-        this.pages = [];
+        this.pages = Array.from({length: pageCount}, () => ({text: "", image: null}));
+        this.currentPage = 1;
+    }
 
-        for (let i = 0; i < pageCount; i++) {
-            this.pages.push({text: "", image: null});
+
+    async getPageContent(title, pageNumber) {
+        try {
+            if (pageNumber > this.pageCount || pageNumber < 1) return "Page not found";
+
+            const response = await fetch(`../book_contents/${this.title}/page_${pageNumber}.txt`);
+            if (!response.ok) throw new Error("Failed to load text file");
+            let text = await response.text();
+
+            return this.formatText(text, 23);
+
+        } catch (error) {
+            console.error("Error loading text:", error);
+            return "Error loading text";
         }
-
     }
 
-    setText(pageNumber, text) {
-        this.pages[pageNumber].text = text;
+    formatText(text, maxChars) {
+        let words = text.split(" ");
+        let formattedText = "";
+        let line = "";
+
+        words.forEach(word => {
+            if ((line + word).length > maxChars) {
+                formattedText += line.trim() + "\n";
+                line = "";
+            }
+            line += word + " ";
+        });
+        formattedText += line.trim();
+        return formattedText;
     }
 
-    setImage(pageNumber, imagePath) {
-        this.pages[pageNumber].image = imagePath;
-    }
-
-    getPageContent(pageNumber) {
-        // const pages = new BABYLON.Layer('', '');
-
-        return this.pages[pageNumber];
-    }
-
-
-}
-
-function bookContents() {
 
 }
 
@@ -39,7 +51,7 @@ function bookContents() {
 
 let bookGUI = null;
 let bookOpen = false;
-let currentBook = null;
+let book = null;
 
 export function openBook(scene, title, pageCount) {
 
@@ -47,11 +59,8 @@ export function openBook(scene, title, pageCount) {
         return;
     }
     bookOpen = true;
-    currentBook = {title, pageCount, currentPage: 1};
-
-    isBookOpen(scene);
-
-    // const book = new interactableBook(title, pageCount);
+    book = new interactableBook(title, pageCount);
+    isBookOpen(book, scene);
 }
 
 export function closeBook() {
@@ -60,11 +69,11 @@ export function closeBook() {
         bookGUI = null;
     }
     bookOpen = false;
-    currentBook = null;
+    book = null;
     console.log("Closed book");
 }
 
-export function isBookOpen(scene) {
+export async function isBookOpen(scene) {
     console.log("checking book status");
 
     if (!bookOpen) {
@@ -79,29 +88,69 @@ export function isBookOpen(scene) {
     bookBackground.stretch = GUI.Image.STRETCH_UNIFORM;
     bookGUI.addControl(bookBackground);
 
-    let prevButton = new GUI.Image("prevButton", "../images-objects/book/left_button.jpg");
-    prevButton.width =  "4%";
-    prevButton.height = "4%";
-    prevButton.stretch = GUI.Image.STRETCH_UNIFORM;
+    let bookContainer = new GUI.Rectangle();
+    bookContainer.width = bookBackground.width;
+    bookContainer.height = bookBackground.height;
+    bookContainer.thickness = 0;
+    bookContainer.isPointerBlocker = true;
+    bookGUI.addControl(bookContainer);
+    bookContainer.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+    bookContainer.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
 
-    prevButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-    prevButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
-    prevButton.left = "24%";
-    prevButton.top = "-18%";
+    function createButton(name, imagePath, alignment, offsetX) {
+        let button = new GUI.Image(name, imagePath);
+        button.width = "6%";
+        button.height = "6%";
+        button.stretch = GUI.Image.STRETCH_UNIFORM;
+        button.horizontalAlignment = alignment;
+        button.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button.left = offsetX;
+        button.top = "-21%";
+        return button;
+    }
 
-    bookGUI.addControl(prevButton);
+    let prevButton = createButton("prevButton", "../images-objects/book/left_button.jpg", GUI.Control.HORIZONTAL_ALIGNMENT_LEFT, "5%");
+    let nextButton = createButton("nextButton", "../images-objects/book/right_button.jpg", GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT, "-5%");
 
-    let nextButton = new GUI.Image("nextButton", "../images-objects/book/right_button.jpg");
-    nextButton.width =  "4%";
-    nextButton.height = "4%";
-    nextButton.stretch = GUI.Image.STRETCH_UNIFORM;
+    bookContainer.addControl(prevButton);
+    bookContainer.addControl(nextButton);
 
-    nextButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
-    nextButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
-    nextButton.left = "-24%";
-    nextButton.top = "-18%";
+    let fontStyle = document.createElement("style");
+    fontStyle.innerHTML = `
+        @font-face {
+            font-family: 'Minecraft';
+            src: url('../fonts/minecraft_font.ttf') format('truetype');
+        }`;
+    document.head.appendChild(fontStyle);
 
-    bookGUI.addControl(nextButton);
+    function createTextBlock(name, offsetX) {
+        let textBlock = new GUI.TextBlock(name);
+        textBlock.width = "60%";
+        textBlock.height = "50%";
+        textBlock.color = "black";
+        textBlock.fontSize = "3.6%";
+        textBlock.fontFamily = "Minecraft";
+        textBlock.textWrapping = true;
+        textBlock.textHorizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+        textBlock.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+        textBlock.top = "-18.25%";
+        textBlock.left = offsetX; // Dynamic position
+        return textBlock;
+    }
+
+    let leftPageText = createTextBlock("leftPageText", "-17.5%");
+    let rightPageText = createTextBlock("rightPageText", "17.5%");
+
+    bookGUI.addControl(leftPageText);
+    bookGUI.addControl(rightPageText);
+
+
+    async function updatePageContent() {
+        leftPageText.text = await book.getPageContent(book.title, book.currentPage);
+        rightPageText.text = await book.getPageContent(book.title, book.currentPage + 1);
+    }
+    
+    await updatePageContent();
 
 
 
